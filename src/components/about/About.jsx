1ea31597ba6a92bdf2be5706d12ddf4986c33b04
AM. Sharfras <sarfiras_ct16062@stu.kln.ac.lk>
2022-08-30 16:15:37 +0530

    finalized about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,6 +26,24 @@ const About = () => {
               <p>56 - 19 - 148</p>
             </div>
           </div>
+          
+          <p>
+            These measurements mean lens width, bridge, and temple length in
+            millimetres (mm). They can be found on the inside of the temple arm.
+            If you already have a pair that you like, you can compare it to
+            these measurements.
+          </p>
+          <div className="p-boder">
+            <p>
+              Filter category 3-General purpose sunglasses. High protection
+              against sunglare. Good UV Protection. Not suitable for driving in
+              twilight, at night or under dull conditions
+            </p>
+            <p>
+              This frame comes with polarised lenses. An additional $100 will be
+              added to your order total for prescription orders.
+            </p>
+          </div>
         </div>
       </div>
     </div>
